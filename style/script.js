7335de43cde7e38daab26f6e13d3f902864bf28d
Bunny-Is-Me <tbao3515@gmail.com>
2026-09-16 05:05:18 +0700

    script.js

--- style/script.js
+++ style/script.js
@@ -8,12 +8,12 @@ const sound3 = document.getElementById('bg-music3');
 const sound4 = document.getElementById('bg-music4');
 
 const content = [
-  { gif: 'https://i.pinimg.com/originals/7a/ef/73/7aef734a86dce4dc206976d4f0586f2c.gif', message: 'Chị chắc chứ? 😢' },
+  { gif: 'https://i.pinimg.com/originals/7a/ef/73/7aef734a86dce4dc206976d4f0586f2c.gif', message: 'Bạn chắc chứ? 😢' },
   { gif: 'https://i.pinimg.com/originals/c8/07/e2/c807e26d8aed392f172f0bf441f60626.gif', message: 'Thử nghĩ lại nha 🥺' },
-  { gif: 'https://i.pinimg.com/originals/0d/ac/7e/0dac7e14010362ff081e2167be218341.gif', message: 'Đừng mà, cho em cơ hội đi 💔' },
+  { gif: 'https://i.pinimg.com/originals/0d/ac/7e/0dac7e14010362ff081e2167be218341.gif', message: 'Đừng mà, cho tớ cơ hội đi 💔' },
   { gif: 'https://i.pinimg.com/originals/88/e7/86/88e786492cc527584feee199936813dd.gif', message: 'Thiệt luôn đó hả? 😭' },
   { gif: 'https://i.pinimg.com/originals/82/be/ae/82beaeb21c686871437f88bbc1593288.gif', message: 'Một lần nữa thôi, năn nỉ đó 😞' },
-  { gif: 'https://i.pinimg.com/originals/97/91/de/9791de11497556c4a5e800427c48fc47.gif', message: 'Em buồn á nha... 😔' },
+  { gif: 'https://i.pinimg.com/originals/97/91/de/9791de11497556c4a5e800427c48fc47.gif', message: 'Tui buồn á nha... 😔' },
 ];
 
 let clickCount = 0;
@@ -78,7 +78,7 @@ noBtn.addEventListener('click', () => {
 });
 
 yesBtn.addEventListener('click', () => {
-  question.textContent = 'Từ nay em có quyền nhớ chị mỗi ngày rồi nha 😌💕';
+  question.textContent = 'Từ nay tớ có quyền nhớ cậu mỗi ngày và hôn cậu mỗi buổi sáng thức dậy rồi nha 💕';
   gif.src = 'https://i.pinimg.com/originals/7e/f6/9c/7ef69cd0a6b0b78526c8ce983b3296fc.gif';
   noBtn.style.display = 'none';
   yesBtn.style.display = 'none';
